24455f47f9e22734d09c73bad323d598a432b859
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2020-10-28 16:05:10 -0400

    Update scripts.js
    Created a function that displays pokemon from repository on buttons in list form

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,18 @@ let pokemonRepository = (function () {
     function add(pokemon) {
         pokemonList.push(pokemon);
     }
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listPokemon = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listPokemon.appendChild(button);
+        pokemonList.appendChild(listPokemon);
+        button.addEventListener('click', function (event) {
+            showDetails(pokemon);
+        });
+    }
     return {
         getAll: getAll,
         add: add
