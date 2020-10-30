93b159df7b62f49d11d6969a74e08e542cac5198
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2020-10-30 14:40:34 -0400

    Added functions to load pokemon list and details from api

--- js/scripts.js
+++ js/scripts.js
@@ -22,10 +22,39 @@ let pokemonRepository = (function () {
             showDetails(pokemon);
         });
     }
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+        return response.json();
+        }).then(function (json) {
+        json.results.forEach(function (item) {
+            let pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+            };
+            add(pokemon);
+        });
+        }).catch(function (e) {
+        console.error(e);
+        })
+    }
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+        return response.json();
+        }).then(function (details) {
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+        }).catch(function (e) {
+        console.error(e);
+        });
+    }
     return {
         getAll: getAll,
         add: add,
         addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
         showDetails: showDetails
     };
 })();
